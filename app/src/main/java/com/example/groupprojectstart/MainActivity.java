08cc2d1f6f92e93d3160702eba453db60e57ea4c
rosstox26 <rosstox26@gmail.com>
2019-11-19 17:03:11 -0500

    Created dummy menu and edited UI on mainactivity

--- app/src/main/java/com/example/groupprojectstart/MainActivity.java
+++ app/src/main/java/com/example/groupprojectstart/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.groupprojectstart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,8 +22,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonSignIn = findViewById(R.id.buttonSignIn);
-        buttonSignUp = findViewById(R.id.buttonSignUp);
+        buttonSignIn = findViewById(R.id.buttonStudentSignIn);
+        buttonSignUp = findViewById(R.id.buttonStudentSignUp);
         buttonForgotPassword = findViewById(R.id.buttonForgotPassword);
         editTextPassword = findViewById(R.id.editTextPassword);
         editTextUsername = findViewById(R.id.editTextUsername);
@@ -33,5 +38,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         //need if statements for determining student or counselor account
 
+    }
+
+//Inserting Dummy Navigation for Development Stages
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.dummymenu, menu);
+        return  super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public  boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.CounselorAvailability) {
+            Intent HomeIntent = new Intent(this, CounselorAvailability.class);
+            startActivity(HomeIntent);
+        }
+        else if (item.getItemId() == R.id.CounselorCheckInManager){
+            Intent SettingsIntent = new Intent(this, CounselorCheckInManager.class);
+            startActivity(SettingsIntent);
+        }
+        else if (item.getItemId() == R.id.CounselorHome){
+            Intent CheckInManagerIntent = new Intent(this,CounselorHome.class);
+            startActivity(CheckInManagerIntent);
+        }
+        else if (item.getItemId() == R.id.MainActivity){
+            Intent MyAvailabilityIntent = new Intent(this, MainActivity.class);
+            startActivity(MyAvailabilityIntent);
+        }
+        else if (item.getItemId() == R.id.StudentApptConfirmation){
+            Intent MyAvailabilityIntent = new Intent(this, StudentApptConfirmation.class);
+            startActivity(MyAvailabilityIntent);
+        }
+        else if (item.getItemId() == R.id.StudentCheckIn){
+            Intent MyAvailabilityIntent = new Intent(this, StudentCheckIn.class);
+            startActivity(MyAvailabilityIntent);
+        }
+        else if (item.getItemId() == R.id.StudentHome){
+            Intent MyAvailabilityIntent = new Intent(this, StudentHome.class);
+            startActivity(MyAvailabilityIntent);
+        }
+        else if (item.getItemId() == R.id.StudentScheduler){
+            Intent MyAvailabilityIntent = new Intent(this, StudentScheduler.class);
+            startActivity(MyAvailabilityIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/groupprojectstart/StudentCheckIn.java
+++ app/src/main/java/com/example/groupprojectstart/StudentCheckIn.java
@@ -30,23 +30,47 @@ public class StudentCheckIn extends AppCompatActivity implements View.OnClickLis
 
     }
 
-    @Override
+    //Inserting Dummy Navigation for Development Stages
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.studentmenu, menu);
+        inflater.inflate(R.menu.dummymenu, menu);
         return  super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public  boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.itemHome) {
-            Intent HomeIntent = new Intent(this, StudentHome.class);
+        if (item.getItemId() == R.id.CounselorAvailability) {
+            Intent HomeIntent = new Intent(this, CounselorAvailability.class);
             startActivity(HomeIntent);
-        } else if (item.getItemId() == R.id.itemSettings){
-        //I linked it back to home since we don't have a settings page
-        Intent SettingsIntent = new Intent(this, StudentHome.class);
-        startActivity(SettingsIntent);
-    }
+        }
+        else if (item.getItemId() == R.id.CounselorCheckInManager){
+            Intent SettingsIntent = new Intent(this, CounselorCheckInManager.class);
+            startActivity(SettingsIntent);
+        }
+        else if (item.getItemId() == R.id.CounselorHome){
+            Intent CheckInManagerIntent = new Intent(this,CounselorHome.class);
+            startActivity(CheckInManagerIntent);
+        }
+        else if (item.getItemId() == R.id.MainActivity){
+            Intent MyAvailabilityIntent = new Intent(this, MainActivity.class);
+            startActivity(MyAvailabilityIntent);
+        }
+        else if (item.getItemId() == R.id.StudentApptConfirmation){
+            Intent MyAvailabilityIntent = new Intent(this, StudentApptConfirmation.class);
+            startActivity(MyAvailabilityIntent);
+        }
+        else if (item.getItemId() == R.id.StudentCheckIn){
+            Intent MyAvailabilityIntent = new Intent(this, StudentCheckIn.class);
+            startActivity(MyAvailabilityIntent);
+        }
+        else if (item.getItemId() == R.id.StudentHome){
+            Intent MyAvailabilityIntent = new Intent(this, StudentHome.class);
+            startActivity(MyAvailabilityIntent);
+        }
+        else if (item.getItemId() == R.id.StudentScheduler){
+            Intent MyAvailabilityIntent = new Intent(this, StudentScheduler.class);
+            startActivity(MyAvailabilityIntent);
+        }
         return super.onOptionsItemSelected(item);
     }
 
