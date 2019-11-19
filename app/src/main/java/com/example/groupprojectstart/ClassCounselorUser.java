d2bfcfbef12c1644e440077e6be95028d8e15186
rosstox26 <rosstox26@gmail.com>
2019-11-19 09:02:49 -0500

    Created CounselorUser Class

---
+++ app/src/main/java/com/example/groupprojectstart/ClassCounselorUser.java
@@ -0,0 +1,18 @@
+package com.example.groupprojectstart;
+
+public class ClassCounselorUser {
+    public String UserID;
+    public String FirstName;
+    public String LastName;
+    public String Email;
+
+    public ClassCounselorUser(String UserID, String FirstName, String LastName, String Email){
+        this.UserID = UserID;
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.Email = Email;
+    }
+
+
+}
+
