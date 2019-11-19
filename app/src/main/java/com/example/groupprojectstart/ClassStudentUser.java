0952f72c90a031841dc31fac8635474ed54bd19d
rohan <rdash@umich.edu>
2019-11-19 09:19:12 -0500

    Hey it's Rohan I added the classStudentUser

---
+++ app/src/main/java/com/example/groupprojectstart/ClassStudentUser.java
@@ -0,0 +1,16 @@
+package com.example.groupprojectstart;
+
+public class ClassStudentUser {
+
+    public String studentUserID;
+    public String studentFirstName;
+    public String studentLastName;
+    public String studentEmail;
+
+    public ClassStudentUser(String studentUserID, String studentFirstName, String studentLastName, String studentEmail) {
+        this.studentUserID = studentUserID;
+        this.studentFirstName = studentFirstName;
+        this.studentLastName = studentLastName;
+        this.studentEmail = studentEmail;
+    }
+}
