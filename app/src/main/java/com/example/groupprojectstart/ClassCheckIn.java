2708f8392277cf2ad9a06cb50439cb0b5a22c61c
Amy <amyorr@umich.edu>
2019-11-19 09:14:53 -0500

    amy check in class

--- app/src/main/java/com/example/groupprojectstart/ClassCheckIn.java
+++ app/src/main/java/com/example/groupprojectstart/ClassCheckIn.java
@@ -2,17 +2,17 @@ package com.example.groupprojectstart;
 
 public class ClassCheckIn {
 
-    public String CheckinID;
-    public String StudentUserID;
-    public String Rating;
-    public String CheckinPushTS;
-    public String CheckinCompleteTS;
+    public String checkinID;
+    public String studentUserID;
+    public String rating;
+    public String checkinPushTS;
+    public String checkinCompleteTS;
 
     public ClassCheckIn(String checkinID, String studentUserID, String rating, String checkinPushTS, String checkinCompleteTS) {
-        CheckinID = checkinID;
-        StudentUserID = studentUserID;
-        Rating = rating;
-        CheckinPushTS = checkinPushTS;
-        CheckinCompleteTS = checkinCompleteTS;
+        this.checkinID = checkinID;
+        this.studentUserID = studentUserID;
+        this.rating = rating;
+        this.checkinPushTS = checkinPushTS;
+        this.checkinCompleteTS = checkinCompleteTS;
     }
 }
