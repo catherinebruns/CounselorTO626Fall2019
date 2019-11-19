3ab74104c777b40d7c37f7f1deb105241b4baeb7
Don Borschel <borsched@gmail.com>
2019-11-19 09:30:35 -0500

    Class - appointment slot V2

--- app/src/main/java/com/example/groupprojectstart/ClassAppointmentSlots.java
+++ app/src/main/java/com/example/groupprojectstart/ClassAppointmentSlots.java
@@ -1,32 +1,43 @@
 package com.example.groupprojectstart;
 
+import java.util.Date;
+
 public class ClassAppointmentSlots {
 
     public String AppointmentID;
-    public String AppointmentDay;
-    public String AppointmentStartTime;
-    public String AppointmentEndTime;
+    //public String AppointmentDay;
+    public Date AppointmentStart =  new Date();
+    public Date AppointmentEnd = new Date();
+
+ //   public String AppointmentStartTime;
+ //   public String AppointmentEndTime;
     public String AppointmentStatus;
     public String AppointmentStudentUserID;
     public String AppointmentCounselorUserID;
     public String AppointmentReason;
-    public String AppointmentCreationTimestamp;
+    public Date AppointmentCreationTimestamp =  new Date();
 
-    public AppointmentSlots() {
+    public ClassAppointmentSlots() {
     }
 
-    public AppointmentSlots(String appointmentID, String appointmentDay, String appointmentStartTime, String appointmentEndTime,
-                            String appointmentStatus, String appointmentStudentUserID, String appointmentCounselorUserID,
-                            String appointmentReason, String appointmentCreationTimestamp) {
+    public ClassAppointmentSlots(Date appointmentStart, Date appointmentEnd, Date AppointmentCreationTimestamp) {
+        AppointmentStart = appointmentStart;
+        AppointmentEnd = appointmentEnd;
+
+    }
+
+    public ClassAppointmentSlots(Date appointmentCreationTimestamp) {
+        AppointmentCreationTimestamp = appointmentCreationTimestamp;
+    }
+
+    public ClassAppointmentSlots(String appointmentID, String appointmentStatus, String appointmentStudentUserID,
+                                 String appointmentCounselorUserID, String appointmentReason) {
+
         AppointmentID = appointmentID;
-        AppointmentDay = appointmentDay;
-        AppointmentStartTime = appointmentStartTime;
-        AppointmentEndTime = appointmentEndTime;
         AppointmentStatus = appointmentStatus;
         AppointmentStudentUserID = appointmentStudentUserID;
         AppointmentCounselorUserID = appointmentCounselorUserID;
         AppointmentReason = appointmentReason;
-        AppointmentCreationTimestamp = appointmentCreationTimestamp;
     }
 
 
