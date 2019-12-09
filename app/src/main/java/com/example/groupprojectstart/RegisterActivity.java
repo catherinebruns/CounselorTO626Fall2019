8cc18409f003d81d424b538fc217f6015b91b82d
nryotoo1 <g610305@yahoo.co.jp>
2019-12-08 20:01:39 -0500

    //change register message

--- app/src/main/java/com/example/groupprojectstart/RegisterActivity.java
+++ app/src/main/java/com/example/groupprojectstart/RegisterActivity.java
@@ -68,10 +68,12 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             //if login is successful
-                            Toast.makeText(RegisterActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegisterActivity.this, "Register Successful", Toast.LENGTH_SHORT).show();
                             //creating a new intent to send you back to the main activity once registration is successful
                             Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
                             startActivity(intent);
+
+
 
                         } else {
                             //if login is unsuccessful
